c39eb2978a1079161188b3511e9d94e04f0c141e
devHikary <wrkkarina@gmail.com>
2021-10-10 17:14:21 -0300

    tags

---
+++ src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
@@ -0,0 +1,37 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LeilaoSteps {
+	@Dado("um usuario logado")
+	public void um_usuario_logado() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Quando("acessa a pagina de novo leilao")
+	public void acessa_a_pagina_de_novo_leilao() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Quando("prenche o formulario com dados validos")
+	public void prenche_o_formulario_com_dados_validos() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Entao("volta para a pagina de leiloes")
+	public void volta_para_a_pagina_de_leiloes() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Entao("o novo leilao aparece na tabela")
+	public void o_novo_leilao_aparece_na_tabela() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
